39219bb917edca39635dc70804772209d3ca482b
om mulge <ommulge@gmail.com>
2026-03-10 17:25:00 +0530

    script

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-ConvolverNode.apply.apply.apply
+// get elements
+let imgBox = document.getElementById("imgBox");
+let qrImage = document.getElementById("qrImage");
+let qrText = document.getElementById("qrText");
+
+// function for generation QR code
+function generateQR() {
+
+    if (qrText.value.length > 0) {
+        // calling API URL and adding it as an image
+        qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
+
+        // showing QR code
+        imgBox.classList.add("show-img");
+    }
+    else {
+        // if text or url is empty
+        qrText.classList.add("error");
+        setTimeout(() => {
+            // shake animation for 10 times
+            qrText.classList.remove("error");
+        }, 1000);
+    }
+}
